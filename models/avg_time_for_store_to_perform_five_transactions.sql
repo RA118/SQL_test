529a7b5aa10903a8a94a82649d7bc941e57aa11a
RA118 <alexei.rzhanov@gmail.com>
2024-09-11 12:10:34 +0200

    Update avg_time_for_store_to_perform_five_transactions.sql

--- models/avg_time_for_store_to_perform_five_transactions.sql
+++ models/avg_time_for_store_to_perform_five_transactions.sql
@@ -1,34 +1,32 @@
---Average time for a store to perform its 5 first transactions
-
 SELECT AVG(diff_hours) AS avg_time --overall avg
 FROM
 	(SELECT 
 	-- here we calculate differences
-		s.id AS store_id
-		s.name AS store_name,
-		, MAX(happened_at_date) OVER (PARTITION BY store_id) AS fifth_transaction_time -- for presentation and qa purpose
-		, MIN(happened_at_date) OVER (PARTITION BY store_id) AS first_transaction_time -- for presentation and qa purpose
+		store_id
+		, store_name
+		, MAX(happened_at) OVER (PARTITION BY store_id) AS fifth_transaction_time -- for presentation and qa purpose
+		, MIN(happened_at) OVER (PARTITION BY store_id) AS first_transaction_time -- for presentation and qa purpose
 		, DATETIME_DIFF(
-			MAX(happened_at_date) OVER (PARTITION BY store_id) 
-		  , MIN(happened_at_date) OVER (PARTITION BY store_id)
+				MAX(happened_at) OVER (PARTITION BY store_id) 
+		  , MIN(happened_at) OVER (PARTITION BY store_id)
 		  , HOUR
 		) AS diff_hours -- DATETIME_DIFF is BQ function, might be different in another DB
 		FROM
 			(SELECT
 			-- here we count transactions per store
-			s.id AS store_id
+			s.id AS store_id,
 			s.name AS store_name,
-			happened_at_date,
+			happened_at,
 			ROW_NUMBER() OVER (PARTITION BY s.id ORDER BY happened_at ASC) AS nth_transaction,
 			COUNT(*) OVER (PARTITION BY s.id) AS max_transactions_per_store
 
-			FROM transaction t
-			LEFT JOIN device d ON t.device_id = d.id
-			LEFT JOIN store s ON d.store_id = s.id
+			FROM sandbox.transactions t
+			LEFT JOIN sandbox.device d ON t.device_id = d.id
+			LEFT JOIN sandbox.store s ON d.store_id = s.id
 
 			WHERE LOWER(status) = 'accepted'
 			--AND DATE(t.happened_at) BETWEEEN DATETRUNC(TODAY(), month) AND TODAY()
-			GROUP BY 1,2
+			GROUP BY 1,2,3
 			--ORDER BY 1,3 DESC;
 			)
 	WHERE max_transactions_per_store >=5 -- stores have 5 or more transactions
