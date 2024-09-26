180b24456117ee212917b630dfd6c58987636498
RA118 <alexei.rzhanov@gmail.com>
2024-09-26 10:48:42 +0200

    Add files via upload

---
+++ target/run/sql_test/models/avg_time_for_store_to_perform_five_transactions.sql
@@ -0,0 +1,47 @@
+
+  
+    
+
+    create or replace table `clean-wonder-435309-m2`.`dbt_ra118`.`avg_time_for_store_to_perform_five_transactions`
+      
+    
+    
+
+    OPTIONS()
+    as (
+      SELECT AVG(diff_hours) AS avg_time --overall avg
+FROM
+	(SELECT 
+	-- here we calculate differences
+		store_id
+		, store_name
+		, MAX(happened_at) OVER (PARTITION BY store_id) AS fifth_transaction_time -- for presentation and qa purpose
+		, MIN(happened_at) OVER (PARTITION BY store_id) AS first_transaction_time -- for presentation and qa purpose
+		, DATETIME_DIFF(
+				MAX(happened_at) OVER (PARTITION BY store_id) 
+		  , MIN(happened_at) OVER (PARTITION BY store_id)
+		  , HOUR
+		) AS diff_hours -- DATETIME_DIFF is BQ function, might be different in another DB
+		FROM
+			(SELECT
+			-- here we count transactions per store
+			s.id AS store_id,
+			s.name AS store_name,
+			happened_at,
+			ROW_NUMBER() OVER (PARTITION BY s.id ORDER BY happened_at ASC) AS nth_transaction,
+			COUNT(*) OVER (PARTITION BY s.id) AS max_transactions_per_store
+
+			FROM sandbox.transactions t
+			LEFT JOIN sandbox.device d ON t.device_id = d.id
+			LEFT JOIN sandbox.store s ON d.store_id = s.id
+
+			WHERE LOWER(status) = 'accepted'
+			--AND DATE(t.happened_at) BETWEEEN DATETRUNC(TODAY(), month) AND TODAY()
+			GROUP BY 1,2,3
+			--ORDER BY 1,3 DESC;
+			)
+	WHERE max_transactions_per_store >=5 -- stores have 5 or more transactions
+	AND nth_transaction = 1 OR nth_transaction = 5 -- we need 1st and 5th
+)
+    );
+  
---
+++ target/run/sql_test/models/top_ten_products_sold.sql
@@ -0,0 +1,28 @@
+
+  
+    
+
+    create or replace table `clean-wonder-435309-m2`.`dbt_ra118`.`top_ten_products_sold`
+      
+    
+    
+
+    OPTIONS()
+    as (
+      -- Top 10 products sold
+SELECT
+t.product_sku, -- added because different products might have same name
+t.product_name AS product_name,
+SUM(t.amount) AS transacted_amount
+
+FROM sandbox.transactions t
+--LEFT JOIN device d ON t.device_id = d.id -- don't need them here as all the fields are from t
+--LEFT JOIN store s ON d.store_id = s.id -- don't need them here as all the fields are from t
+
+WHERE LOWER(status) = 'accepted'
+--AND DATE(t.happened_at) BETWEEEN DATETRUNC(TODAY(), MONTH) AND TODAY() -- in real life there will be limitation
+GROUP BY 1,2
+ORDER BY 3 DESC
+LIMIT 10
+    );
+  
